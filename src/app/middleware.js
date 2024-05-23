dd8ac89e1dcc248eb61415e4304b3c4cb2e5eb4b
Akshat Sabharwal <akshatsabharwal35@gmail.com>
2024-05-23 14:09:48 +0530

    dk

--- src/app/middleware.js
+++ src/app/middleware.js
@@ -28,7 +28,7 @@ app.use(expressMongoSanitize());
 app.use(hpp());
 app.use(xss());
 
-app.use(cors({ origin: "http://127.0.0.1:5173", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 if (process.env.NODE_ENV === "production") {
   app.use(
