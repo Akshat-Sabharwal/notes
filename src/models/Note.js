b664307caa6cc2e35c6d978f263d8679b9f25e3a
Akshat Sabharwal <akshatsabharwal35@gmail.com>
2024-05-23 18:31:14 +0530

    Encryption removed

--- src/models/Note.js
+++ src/models/Note.js
@@ -36,54 +36,12 @@ const noteSchema = mongoose.Schema(
 // DOCUMENT MIDDLEWARE
 noteSchema.pre("save", async function (next) {
   this.slug = slugify(this.title).toLowerCase();
-
-  const text = this.text;
-
-  const jsonCipher = cryptoJS.AES.encrypt(
-    JSON.stringify({ text }),
-    process.env.HASH_KEY,
-  ).toString();
-
-  this.text = cryptoJS.enc.Base64.stringify(
-    cryptoJS.enc.Utf8.parse(jsonCipher),
-  );
-
   next();
 });
 
 // QUERY MIDDLEWARE
 noteSchema.pre(/^find/, function (next) {
   this.select("-__v");
-  next();
-});
-
-noteSchema.post("find", function (docs, next) {
-  for (let doc of docs) {
-    let decData = cryptoJS.enc.Base64.parse(doc.text).toString(
-      cryptoJS.enc.Utf8,
-    );
-    let text = cryptoJS.AES.decrypt(
-      decData,
-      process.env.HASH_KEY,
-    ).toString(cryptoJS.enc.Utf8);
-
-    doc.text = JSON.parse(text);
-  }
-
-  next();
-});
-
-noteSchema.post(/^findOne/, function (doc, next) {
-  let decData = cryptoJS.enc.Base64.parse(doc.text).toString(
-    cryptoJS.enc.Utf8,
-  );
-  let text = cryptoJS.AES.decrypt(
-    decData,
-    process.env.HASH_KEY,
-  ).toString(cryptoJS.enc.Utf8);
-
-  doc.text = JSON.parse(text);
-
   next();
 });
 
